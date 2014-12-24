7f2dfac7b80b8ed32a40bb72eaba4025f530611e
Ipalibo <ipalibowhyte@gmail.com>
2014-12-24 03:10:07 +0000

    Landing page all animated

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,32 @@ function creatRoom(){
 function joinRoom(){
 	socket.emit('joinSession', {sessionName: 'RichGang'})
 }
+
+$(document).ready(function(){
+	animateOnLoad();
+});
+
+function animateOnLoad(){
+
+	setTimeout(function(){
+		$('#main').css('visibility', 'visible');
+    	$('#main').animate({opacity: 1}, 3000, function(){
+    	});
+    },500); 
+
+	setTimeout(function(){
+		$('#infoText').css('visibility', 'visible');
+		$('#infoText').addClass('animated fadeInDown');
+	},2000); 
+
+
+	setTimeout(function(){
+		$('#logo').css('display', 'none');
+		$('#logo').css('display', 'inline-block');
+		$('#logo').addClass('animated rubberBand');
+	},6000); 
+
+
+
+	//$('.row#formField').addClass('animated fadeInUp');
+}
